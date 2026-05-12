7739772f4556dfbf3cd50f755f9c034109a1519b
RedStar <red.star@gmail.com>
2026-05-12 02:15:02 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/_RedStar/Audio/Jukebox/JukeboxVolume.cs
+++ Content.Shared/_RedStar/Audio/Jukebox/JukeboxVolume.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2026 RedStar Contributors
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 namespace Content.Shared._RedStar.Audio.Jukebox;
 
 public static class JukeboxVolume
