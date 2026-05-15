e7324587f7d61bdf603d4d8d52d5be239b5d1ada
RedStar <red.star@gmail.com>
2026-05-15 09:19:18 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Client/Patron/PatronSupportSystem.cs
+++ Content.Goobstation.Client/Patron/PatronSupportSystem.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2026 Goob Station Contributors
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Client.Lobby;
 using Content.Goobstation.Common.CCVar;
 using Content.Shared.CCVar;
