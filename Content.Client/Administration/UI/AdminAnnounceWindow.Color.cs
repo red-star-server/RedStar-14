90b1bb5a86586427bd2ec748a048b7c0bdda695c
RedStar <red.star@gmail.com>
2026-05-15 10:20:58 +0000

    chore: Automatically update REUSE headers

--- Content.Client/Administration/UI/AdminAnnounceWindow.Color.cs
+++ Content.Client/Administration/UI/AdminAnnounceWindow.Color.cs
@@ -1,4 +1,8 @@
-﻿using Content.Shared.Administration;
+// SPDX-FileCopyrightText: 2026 RedStar Contributors
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+using Content.Shared.Administration;
 
 namespace Content.Client.Administration.UI;
 
