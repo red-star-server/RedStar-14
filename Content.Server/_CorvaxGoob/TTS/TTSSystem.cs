8578bbdae8bd40605bf0b37a4718234f72f5a16d
RedStar <red.star@gmail.com>
2026-05-15 11:09:08 +0000

    chore: Automatically update REUSE headers

--- Content.Server/_CorvaxGoob/TTS/TTSSystem.cs
+++ Content.Server/_CorvaxGoob/TTS/TTSSystem.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2026 RedStar Contributors
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using System.Threading.Tasks;
 using Content.Server._EinsteinEngines.Language;
 using Content.Server.Chat.Systems;
