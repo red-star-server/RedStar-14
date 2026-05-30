bd64125cdd43b08413e26e69f5f768ba8544df3a
RedStar <red.star@gmail.com>
2026-05-30 18:15:06 +0000

    chore: Automatically update REUSE headers

--- Content.Server/_RedStar/Skills/SkillsSystem.cs
+++ Content.Server/_RedStar/Skills/SkillsSystem.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2026 RedStar Contributors
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using System.Linq;
 using Content.Server.Administration.Managers;
 using Content.Server.Antag.Components;
