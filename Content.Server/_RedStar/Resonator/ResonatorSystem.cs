242de0199b294c49b53fe38eec6cbf53d305e4fd
RedStar <red.star@gmail.com>
2026-06-12 10:22:52 +0000

    chore: Automatically update REUSE headers

--- Content.Server/_RedStar/Resonator/ResonatorSystem.cs
+++ Content.Server/_RedStar/Resonator/ResonatorSystem.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2026 RedStar Contributors
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Server.Destructible;
 using Content.Server.Gatherable;
 using Content.Server.Gatherable.Components;
