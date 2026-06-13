5e1d740c40e736e17bb7cdbd9ce6d9df81f28f0a
RedStar <red.star@gmail.com>
2026-06-13 07:40:03 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Server/StationEvents/GameDirector/GameDirectorSystem.Story.cs
+++ Content.Goobstation.Server/StationEvents/GameDirector/GameDirectorSystem.Story.cs
@@ -1,4 +1,8 @@
-﻿using System.Linq;
+// SPDX-FileCopyrightText: 2026 Goob Station Contributors
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+using System.Linq;
 using Content.Goobstation.Server.StationEvents.Components;
 using Content.Goobstation.Server.StationEvents.Metric;
 using Content.Server.StationEvents.Components;
