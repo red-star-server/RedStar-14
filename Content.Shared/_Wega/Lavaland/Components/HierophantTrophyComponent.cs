71561a12e6789a76dc7a01c538da529657d5fa2d
Fenriz <kastonbag552@gmail.com>
2026-06-19 20:32:53 +0300

    4

---
+++ Content.Shared/_Wega/Lavaland/Components/HierophantTrophyComponent.cs
@@ -0,0 +1,14 @@
+// Все модификации и наработки в ss14-wega под тегом Corvax-Wega и директориях _Wega лицензированы под GNU GPL v3.
+// https://github.com/corvax-team/ss14-wega/blob/master/LICENSE.TXT
+
+using Robust.Shared.GameStates;
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared._Wega.Lavaland.Components;
+
+[RegisterComponent, NetworkedComponent]
+public sealed partial class HierophantTrophyComponent : Component
+{
+    [DataField]
+    public EntProtoId WallPrototype = "WallHierophantTrophy";
+}
