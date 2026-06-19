ef6b09d7c5e573eb864627ec2def1292b42058de
RedStar <red.star@gmail.com>
2026-06-19 17:48:10 +0000

    chore: Automatically update REUSE headers

--- Content.Server/_Wega/Lavaland/Components/Mobs/Bubblegum/BubblegumPhaseEnum.cs
+++ Content.Server/_Wega/Lavaland/Components/Mobs/Bubblegum/BubblegumPhaseEnum.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2026 RedStar Contributors
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 // Все модификации и наработки в ss14-wega под тегом Corvax-Wega и директориях _Wega лицензированы под GNU GPL v3.
 // https://github.com/corvax-team/ss14-wega/blob/master/LICENSE.TXT
 
