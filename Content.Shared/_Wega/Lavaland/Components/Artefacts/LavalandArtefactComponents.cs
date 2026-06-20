a750805cf206e8834a611b974120ea2b452c6a1d
Fenriz <kastonbag552@gmail.com>
2026-06-20 04:25:06 +0300

    5

---
+++ Content.Shared/_Wega/Lavaland/Components/Artefacts/LavalandArtefactComponents.cs
@@ -0,0 +1,70 @@
+// Все модификации и наработки в ss14-wega под тегом Corvax-Wega и директориях _Wega
+// лицензированы под GNU GPL v3:
+// https://github.com/corvax-team/ss14-wega/blob/master/LICENSE.TXT
+
+using Content.Shared.Maps;
+using Content.Shared.DoAfter;
+using Robust.Shared.Audio;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization;
+
+namespace Content.Shared._Wega.Lavaland.Components.Artefacts;
+
+[RegisterComponent]
+public sealed partial class LavaStaffComponent : Component
+{
+    [DataField]
+    public EntProtoId LavaEntity = "FloorLavaEntity";
+
+    [DataField]
+    public ProtoId<ContentTileDefinition> BasaltTile = "FloorBasaltLavaland";
+
+    [DataField]
+    public SoundSpecifier? UseSound;
+}
+
+[RegisterComponent]
+public sealed partial class DragonBloodComponent : Component
+{
+    [DataField]
+    public TimeSpan UseTime = TimeSpan.FromSeconds(5);
+
+    [DataField]
+    public SoundSpecifier UseSound = new SoundPathSpecifier("/Audio/Items/drink.ogg");
+}
+
+[RegisterComponent]
+public sealed partial class SoulStorageComponent : Component
+{
+    [DataField]
+    public float BonusDamagePerSoul = 4f;
+
+    [DataField]
+    public float MaxBonusDamage = 76f;
+
+    [ViewVariables(VVAccess.ReadOnly)]
+    public HashSet<EntityUid> StolenSouls = [];
+}
+
+[RegisterComponent]
+public sealed partial class DivineVocalCordsImplantComponent : Component
+{
+    [DataField]
+    public float Radius = 5f;
+
+    [DataField]
+    public TimeSpan Cooldown = TimeSpan.FromSeconds(30);
+
+    [ViewVariables]
+    public TimeSpan NextUse;
+}
+
+[RegisterComponent]
+public sealed partial class DivineVoiceCarrierComponent : Component
+{
+    [DataField]
+    public EntityUid Implant;
+}
+
+[Serializable, NetSerializable]
+public sealed partial class DragonBloodDoAfterEvent : SimpleDoAfterEvent;
